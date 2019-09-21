8234ca98f46d37baa4b66987d3613adbd4b74ca2
Ryan Carville <ryancarville@gmail.com>
2019-09-21 14:43:09 +0200

    404

--- src/components/404/404.js
+++ src/components/404/404.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function NoPath() {
     return (
         <section>
-            <h1>404 Error: Path not Found</h1>
+            <h1>404 Error: Path not Found.  Waiting for build.</h1>
         </section>
     )
 }
