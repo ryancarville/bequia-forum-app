1c5bebba33b4b9f1991a88d0299c8ad3bff59a4b
Ryan Carville <ryancarville@gmail.com>
2019-11-20 14:38:45 +0100

    image work

--- src/components/Icons/comment.js
+++ src/components/Icons/comment.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./comment.css";
 const comment = (
   <img
-    src="https://bequiaforum.com/Images/comment.png"
+    src="https://bequiaforum.com/images/comment.png"
     alt="comment-icon"
     id="comment-btn-img"
   />
