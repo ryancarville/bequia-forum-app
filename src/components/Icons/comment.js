75c5f4aa89d11888d5f645f666d90d532be02be5
Ryan Carville <ryancarville@gmail.com>
2019-11-20 14:36:04 +0100

    working on images path

--- src/components/Icons/comment.js
+++ src/components/Icons/comment.js
@@ -1,6 +1,10 @@
 import React from "react";
 import "./comment.css";
 const comment = (
-  <img src="../../icons/comment.png" alt="comment-icon" id="comment-btn-img" />
+  <img
+    src="https://bequiaforum.com/Images/comment.png"
+    alt="comment-icon"
+    id="comment-btn-img"
+  />
 );
 export default comment;
