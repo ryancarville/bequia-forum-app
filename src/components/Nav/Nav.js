b355908897e6650c1acb0bbb0ad7275270cb05d1
Ryan Carville <ryancarville@gmail.com>
2019-11-22 11:15:59 +0100

    corbs testing

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -12,7 +12,7 @@ class Nav extends Component {
           <span className="forumLogo">
             <Link to="/">
               <img
-                src="https://bequiaforum.com/static/media/bequia-logo.png"
+                src="https://bequiaforum.com/images/bequia-logo.png"
                 alt="forum-icon"
               />
             </Link>
@@ -37,8 +37,7 @@ class Nav extends Component {
           <span className="forumLogo">
             <Link to="/">
               <img
-                samesite="none"
-                src="https://bequiaforum.com/static/media/bequia-logo.png"
+                src="https://bequiaforum.com/images/bequia-logo.png"
                 alt="forum-icon"
               />
             </Link>
