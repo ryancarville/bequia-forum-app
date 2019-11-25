09b3061fde0defbf3c99f413bae8d9da64767a88
Ryan Carville <ryancarville@gmail.com>
2019-11-25 19:49:23 +0100

    styling landing page

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -6,18 +6,14 @@ export default function LandingPage() {
   return (
     <section className="landingPage-container">
       <div className="landingPage-content">
-        <img
-          id="palm-trees"
-          src="https://bequiaforum.com/static/media/bequia-image.jpg"
-          alt="palm-trees"
-        />
+        <div id="palm-trees" alt="palm-trees" />
         <div id="landing-page-text">
           <h1>Welcome to Bequia Forum!</h1>
           <article id="landing-page-text-p">
-            <h4>A place to connect with locals, travelers and the world.</h4>
+            <h4>Connect with locals, travelers and the world.</h4>
             <p>
-              You will find message boards on a number of topics, a events
-              calender, job listings, market place and rentals section.
+              Message boards, events calendar, job listings, a market place
+              and rentals section.
               <br /> <Link to="/signup">Create an account</Link> today to get
               the most out of the platform. i.e. Create Posts, events, comments
               etc.
