18e397338468cc49672c119ec4d02c0dddbe8e65
Ryan Carville <ryancarville@gmail.com>
2019-11-29 16:19:13 +0100

    added loaded svg to manifest

--- src/components/Icons/waveLoader.js
+++ src/components/Icons/waveLoader.js
@@ -3,7 +3,7 @@ import "./waveLoader.css";
 const waveLoader = (
   <div className="forum-wave-loader">
     <img
-      src="https://bequiaforum.com/images/JPEG/wave-loader.svg"
+      src="/images/loader/wave-loader.svg"
       alt="wave-loader"
       id="wave-loader-gif"
     />
