65416c7dd989f3a8ee267db71d862abc1a08175f
Ryan Carville <ryancarville@gmail.com>
2019-12-15 12:07:10 +0100

    changed lifecycle in search input

--- src/components/SearchInput/SearchInput.js
+++ src/components/SearchInput/SearchInput.js
@@ -122,7 +122,7 @@ export default class SearchInput extends Component {
       });
     }
   }
-  componentDidUnmount() {
+  componentWillUnmount() {
     this.setState({
       forum: [],
       cats: [],
