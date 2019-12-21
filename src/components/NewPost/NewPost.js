6935f2499848f26b8831dd31426172e832553dca
Ryan Carville <ryancarville@gmail.com>
2019-12-21 22:45:00 +0100

    added paginator to new post

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -1,80 +1,28 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import Truncate from "react-truncate";
 import apiServices from "../../services/apiServices";
-import formatDate from "../../helpers/formatDate";
 import Sort from "../Sort/Sort";
 import "./NewPost.css";
-import like from "../Icons/like";
-import comment from "../Icons/comment";
 import waveLoader from "../Icons/waveLoader";
+import Paginator from "../Paginator/Paginator";
+import MakePostCards from "../MakePostCards/MakePostCards";
+
 //new posts component
 export default class NewPost extends Component {
   constructor(props) {
     super(props);
     this.state = {
       posts: [],
-
+      currentPosts: [],
+      currentPage: null,
+      totalPages: null,
+      pageLimit: 5,
       error: null
     };
   }
-  //get most rest posts
-  recentPosts = () => {
-    return this.state.posts.map(p => {
-      var forum = this.state.forum.filter(f => f.id === p.board_id);
-      forum = forum[0];
-      return (
-        <li key={p.id} className="post-card">
-          <article className="post-card-info">
-            <Link
-              to={`/messageBoard/${forum.messageboard_section}/${p.board_id}/${p.id}`}
-            >
-              <h4>{p.title}</h4>
-            </Link>
-            <Truncate
-              className="post-teaser"
-              lines={2}
-              ellipsis={
-                <span>
-                  ...
-                  <Link
-                    to={`/messageBoard/${forum.messageboard_section}/${p.board_id}/${p.id}`}
-                  >
-                    Read more
-                  </Link>
-                </span>
-              }
-            >
-              {p.content}
-            </Truncate>
-          </article>
-          <span className="post-card-user-info">
-            <p>
-              Posted By:{"   "}
-              {p.user_name}
-            </p>
-            <p>Posted On: {formatDate(p.date_posted)}</p>
-            <span className="post-card-icons">
-              <p>
-                {like}
-                {"   "}
-                {p.likes}
-              </p>
-              <p>
-                {comment}
-                {"   "}
-                {p.commentCount}
-              </p>
-            </span>
-          </span>
-        </li>
-      );
-    });
-  };
 
   handleSort = sort => {
     if (sort.sortType === "asc" && sort.column === "title") {
-      const sorted = this.state.posts.sort(function(a, b) {
+      const sorted = this.state.currentPosts.sort(function(a, b) {
         var x = a.title.toLowerCase();
         var y = b.title.toLowerCase();
         if (x < y) {
@@ -86,12 +34,12 @@ export default class NewPost extends Component {
         return 0;
       });
       this.setState({
-        posts: sorted
+        currentPosts: sorted
       });
       return;
     }
     if (sort.sortType === "desc" && sort.column === "title") {
-      const sorted = this.state.posts.sort(function(a, b) {
+      const sorted = this.state.currentPosts.sort(function(a, b) {
         var x = a.title.toLowerCase();
         var y = b.title.toLowerCase();
         if (x > y) {
@@ -103,12 +51,12 @@ export default class NewPost extends Component {
         return 0;
       });
       this.setState({
-        posts: sorted
+        currentPosts: sorted
       });
       return;
     }
     if (sort.sortType === "asc" && sort.column === "date_posted") {
-      const sorted = this.state.posts.sort(function(a, b) {
+      const sorted = this.state.currentPosts.sort(function(a, b) {
         var x = a.date_posted.toLowerCase();
         var y = b.date_posted.toLowerCase();
         if (x < y) {
@@ -120,12 +68,12 @@ export default class NewPost extends Component {
         return 0;
       });
       this.setState({
-        posts: sorted
+        currentPosts: sorted
       });
       return;
     }
     if (sort.sortType === "desc" && sort.column === "date_posted") {
-      const sorted = this.state.posts.sort(function(a, b) {
+      const sorted = this.state.currentPosts.sort(function(a, b) {
         var x = a.date_posted.toLowerCase();
         var y = b.date_posted.toLowerCase();
         if (x > y) {
@@ -137,25 +85,25 @@ export default class NewPost extends Component {
         return 0;
       });
       this.setState({
-        posts: sorted
+        currentPosts: sorted
       });
       return;
     }
     if (sort.sortType === "asc" && sort.column === "likes") {
-      const sorted = this.state.posts.sort(function(a, b) {
+      const sorted = this.state.currentPosts.sort(function(a, b) {
         return a.likes - b.likes;
       });
       this.setState({
-        posts: sorted
+        currentPosts: sorted
       });
       return;
     }
     if (sort.sortType === "desc" && sort.column === "likes") {
-      const sorted = this.state.posts.sort(function(a, b) {
+      const sorted = this.state.currentPosts.sort(function(a, b) {
         return b.likes - a.likes;
       });
       this.setState({
-        posts: sorted
+        currentPosts: sorted
       });
       return;
     }
@@ -186,26 +134,96 @@ export default class NewPost extends Component {
                 const sorted = this.state.posts.sort((a, b) =>
                   a.date_posted > b.date_posted ? -1 : 1
                 );
-                this.setState({
-                  posts: sorted,
-                  dataLoaded: true
-                });
+
+                this.setState(
+                  {
+                    posts: sorted
+                  },
+                  () => {
+                    if (sorted.length === posts.length) {
+                      this.setState({
+                        dataLoaded: true
+                      });
+                    }
+                  }
+                );
               });
           });
         })
       );
   }
 
+  onPageChanged = data => {
+    const { posts } = this.state;
+    const { currentPage, totalPages, pageLimit } = data;
+    const offset = (currentPage - 1) * pageLimit;
+    const currentPosts = posts.slice(offset, offset + pageLimit);
+
+    this.setState({ currentPage, currentPosts, totalPages });
+  };
+
+  handlePageLimit = e => {
+    this.setState(
+      {
+        pageLimit: parseInt(e.target.value)
+      },
+      () => {
+        console.log(this.state);
+        const paginationData = {
+          currentPage: this.state.currentPage,
+          totalPages: this.state.totalPages,
+          pageLimit: this.state.pageLimit,
+          totalRecords: this.state.totalRecords
+        };
+        this.onPageChanged(paginationData);
+      }
+    );
+  };
   render() {
+    const { posts, currentPosts, currentPage, totalPages } = this.state;
+    const totalPosts = posts.length;
+    if (totalPosts === 0) return null;
+
     return this.state.dataLoaded ? (
       <div className="newPost-container">
         <header>
           {this.props.dashboard ? null : <h2>Newest Posts</h2>}{" "}
           <Sort sortType="posts" handleSort={this.handleSort} />
         </header>
+
+        <p>{this.state.error}</p>
         <div className="newPost-content">
-          <ul>{this.recentPosts()}</ul>
-          <p>{this.state.error}</p>)
+          <div className="paginator-wrapper">
+            {currentPage && (
+              <span className="paginator-current-page">
+                Page <span className="font-weight-bold">{currentPage}</span> /{" "}
+                <span className="font-weight-bold">{totalPages}</span>
+              </span>
+            )}
+            <Paginator
+              totalRecords={totalPosts}
+              pageLimit={this.state.pageLimit}
+              pageNeighbours={1}
+              onPageChanged={this.onPageChanged}
+            />
+            <select
+              className="num-post-results"
+              onChange={this.handlePageLimit}
+            >
+              <option selected disabled value="">
+                Posts per page
+              </option>
+              <option value="5">5</option>
+              <option value="10">10</option>
+              <option value="15">15</option>
+              <option value="20">20</option>
+              <option value="25">25</option>
+              <option value="30">30</option>
+            </select>
+          </div>
+          <ul>
+            <MakePostCards posts={currentPosts} forum={this.state.forum} />
+          </ul>
         </div>
       </div>
     ) : (
